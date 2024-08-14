41bb359b8f9bd7c8ad28de365bacde01e30ee82b
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-08-14 12:32:22 +0530

    nav bar start

---
+++ protfoliyo-site/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+
+    <nav className=".sticky.top-0.z-50.py-3.backdrop-blur-lg.border-b.border-neutral-700/80">
+        <div className="container relative px-4 mx-auto text-sm">
+            <div className="flex items-center justify-center">
+                <div className="flex items-center flex-shrink-0">
+                  <span className="text-xl tracking-tight">Subodha Hettiarachchi</span>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+  ) 
+}
+
+export default Navbar
