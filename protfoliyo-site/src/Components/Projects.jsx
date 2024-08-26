fefdb5bf81e10761f613dc96336878876afcf9dc
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-08-26 23:44:30 +0530

    behance icon update

--- protfoliyo-site/src/Components/Projects.jsx
+++ protfoliyo-site/src/Components/Projects.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import { Github, Figma, Palette } from 'lucide-react';
+import { Github, Figma, } from 'lucide-react';
 import proj1 from "../assets/ProjectsIMG/project test 1.png"
 import proj2 from "../assets/ProjectsIMG/project test 2.png"
+
+
 
 const Projects = () => {
   const [showCard, setShowCard] = useState("all");
@@ -90,7 +92,7 @@ const Projects = () => {
                 imageSrc= {proj1}
                 githubLink="#"
                 figmaLink="#"
-                paletteLink="#"
+                BehanceLink="#"
                 showCard={showCard}
               />
               <PortfolioCard
@@ -101,7 +103,7 @@ const Projects = () => {
                 imageSrc={proj2}
                 githubLink="#"
                 figmaLink="#"
-                paletteLink="#"
+                BehanceLink="#"
                 showCard={showCard}
               />
               <PortfolioCard
@@ -112,7 +114,7 @@ const Projects = () => {
                 imageSrc={proj2}
                 githubLink="#"
                 figmaLink="#"
-                paletteLink="#"
+                BehanceLink="#"
                 showCard={showCard}
               />
               {/* Add more PortfolioCard components as needed */}
@@ -139,7 +141,8 @@ const PortfolioCard = ({
   imageSrc,
   githubLink,
   figmaLink,
-  paletteLink
+  // paletteLink,
+  BehanceLink
 }) => {
   return (
     <div
@@ -159,30 +162,38 @@ const PortfolioCard = ({
           </div>
           <div className="flex flex-wrap items-center mt-6 space-x-2">
   {githubLink && (
-    <a
-      href={githubLink}
-      className="flex items-center px-2 py-1 text-xs text-white transition bg-gray-800 rounded-lg sm:px-3 sm:py-2 md:px-4 md:py-3 sm:text-sm md:text-base hover:bg-gray-700 sm:m-1"
-    >
-      <Github className="w-4 h-4 mr-1 sm:w-5 sm:h-5 md:w-6 md:h-6" />
-      <span className="hidden md:block">GitHub</span>
-    </a>
-  )}
+  <a
+    href={githubLink}
+    className="flex items-center justify-center p-2 text-xs text-white transition bg-gray-800 rounded-lg hover:bg-gray-700 sm:m-1 lg:p-3"
+  >
+    <Github className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6" />
+    <span className="hidden lg:block lg:ml-1">GitHub</span>
+  </a>
+)}
+
   {figmaLink && (
     <a
       href={figmaLink}
-      className="flex items-center px-2 py-1 text-xs text-white transition bg-gray-800 rounded-lg sm:px-3 sm:py-2 md:px-4 md:py-3 sm:text-sm md:text-base hover:bg-gray-700 "
+      className="flex items-center justify-center p-2 text-xs text-white transition bg-gray-800 rounded-lg hover:bg-gray-700 sm:m-1 lg:p-3 "
     >
       <Figma className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6" />
-      <span className="hidden md:block">Figma</span>
+      <span className="hidden md:block lg:ml-1">Figma</span>
     </a>
   )}
-  {paletteLink && (
+  {BehanceLink && (
     <a
-      href={paletteLink}
-      className="flex items-center px-2 py-1 text-xs text-white transition bg-gray-800 rounded-lg sm:px-3 sm:py-2 md:px-4 md:py-3 sm:text-sm md:text-base hover:bg-gray-700"
+      href={BehanceLink}
+      className="flex items-center justify-center p-2 text-xs text-white transition bg-gray-800 rounded-lg hover:bg-gray-700 sm:m-1 lg:p-3"
     >
-      <Palette className="w-4 h-4 mr-1 sm:w-5 sm:h-5 md:w-6 md:h-6" />
-      <span className="hidden md:block lg:mr-1">Behance</span>
+                  <svg
+                  className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  fill="currentColor"
+                >
+                  <path d="M232 237.2c31.8-15.2 48.4-38.2 48.4-74 0-70.6-52.6-87.8-113.3-87.8H0v354.4h171.8c64.4 0 124.9-30.9 124.9-102.9 0-44.5-21.1-77.4-64.7-89.7zM77.9 135.9H151c28.1 0 53.4 7.9 53.4 40.5 0 30.1-19.7 42.2-47.5 42.2h-79v-82.7zm83.3 233.7H77.9V272h84.9c34.3 0 56 14.3 56 50.6 0 35.8-25.9 47-57.6 47zm358.5-240.7H376V94h143.7v34.9zM576 305.2c0-75.9-44.4-139.2-124.9-139.2-78.2 0-131.3 58.8-131.3 135.8 0 79.9 50.3 134.7 131.3 134.7 61.3 0 101-27.6 120.1-86.3H509c-6.7 21.9-34.3 33.5-55.7 33.5-41.3 0-63-24.2-63-65.3h185.1c.3-4.2 .6-8.7 .6-13.2zM390.4 274c2.3-33.7 24.7-54.8 58.5-54.8 35.4 0 53.2 20.8 56.2 54.8H390.4z" />
+                </svg>
+      <span className="hidden md:block lg:ml-1">Behance</span>
     </a>
   )}
 </div>
