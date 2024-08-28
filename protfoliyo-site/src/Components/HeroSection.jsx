98e1f6b79ee642304f35f20c714a08f931226c9e
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-08-28 10:43:57 +0530

    update linkdin icon

--- protfoliyo-site/src/Components/HeroSection.jsx
+++ protfoliyo-site/src/Components/HeroSection.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Download } from "lucide-react";
+import { Download, Linkedin } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const HeroSection = () => {
@@ -29,8 +29,8 @@ const HeroSection = () => {
         
         <div className="flex justify-center">
           <div className="flex justify-center px-5 py-10">
-            <span className="text-xl font-semibold text-transparent bg-gradient-to-r from-orange-500 to-red-800 bg-clip-text lg:text-3xl">
-              UI/UX Engineer
+            <span className="flex flex-wrap items-center justify-center text-orange-500 lg:font-medium bg-gradient-to-r bg-clip-text lg:text-3xl sm:font-medium sm:text-sm">
+              <span>UI/UX Engineer</span><span className="px-3">|</span><span>Software Engineer</span><span className="px-3">|</span><span>Creative Designer</span>
             </span>
           </div>
         </div>
@@ -40,14 +40,25 @@ const HeroSection = () => {
           Welcome to my portfolio, where I showcase my journey and projects in software engineering and design.
         </p>
 
-        <div className="flex justify-center my-10">
-          <button className="relative inline-block p-px font-semibold leading-6 text-white no-underline bg-[#1c1917] shadow-2xl cursor-pointer group rounded-xl shadow-zinc-900">
+        <div className="flex justify-center gap-2 my-10">
+          <button className="relative inline-block p-px md:font-semibold sm:font-medium leading-6 text-white no-underline bg-[#1c1917] shadow-2xl cursor-pointer group rounded-xl shadow-zinc-900">
             <span className="absolute inset-0 overflow-hidden rounded-xl">
               <span className="absolute inset-0 rounded-xl bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(255,63,4,0.6)_0%,rgba(255,165,0,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
             </span>
-            <div className="relative z-10 flex items-center px-6 py-3 space-x-2 rounded-xl bg-gray-950/50 ring-1 ring-white/10">
+            <div className="relative z-10 flex items-center px-5 py-3 space-x-2 rounded-xl bg-gray-950/50 ring-1 ring-white/10">
               <span>Download My CV</span>
               <Download className="w-6 h-6" />
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-gray-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+          </button>
+
+          <button className="relative inline-block p-px md:font-semibold sm:font-medium leading-6 text-white no-underline bg-[#1c1917] shadow-2xl cursor-pointer group rounded-xl shadow-zinc-900">
+            <span className="absolute inset-0 overflow-hidden rounded-xl">
+              <span className="absolute inset-0 rounded-xl bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(255,63,4,0.6)_0%,rgba(255,165,0,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+            </span>
+            <div className="relative z-10 flex items-center px-5 py-3 space-x-2 rounded-xl bg-gray-950/50 ring-1 ring-white/10">
+              
+              <Linkedin className="w-6 h-6" />
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-gray-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
           </button>
